34b19585d4f41cbeb1892920b29128b45f773962
dp_nankov <dimitar.pnankov@gmail.com>
2022-11-19 15:30:18 +0200

    Adding HTML, CSS and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GuestHomeComponent } from './guest-home/guest-home.component';
+import { ItemsComponent } from './items/items.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: GuestHomeComponent
+  },
+  {
+    path: 'items',
+    component: ItemsComponent
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
