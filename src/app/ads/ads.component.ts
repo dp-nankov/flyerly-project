453f90d28f73851edac4ac1d5b6dce3c1c093b63
dp_nankov <dimitar.pnankov@gmail.com>
2022-12-18 15:06:28 +0200

    fix

--- src/app/ads/ads.component.ts
+++ src/app/ads/ads.component.ts
@@ -10,58 +10,54 @@ import { LoaderService } from '../core/loader.service';
   styleUrls: ['./ads.component.scss']
 })
 export class AdsComponent implements OnInit {
-    errorMsg!:string | undefined;
-    loading = true;
+  errorMsg!: string | undefined;
+  loading = true;
 
-    ads!:IAd[];
-    filteredAds:IAd[] = [];
-    filteredAdsP:IAd[] = [];
-    searchTerm: string | null | undefined = '';
-    noResult = false;
-    searchPriceDown: string | null | undefined = '';
-    searchPriceUp: string | null | undefined = '';
+  ads!: IAd[];
+  filteredAds: IAd[] = [];
+  searchTerm: string | null | undefined = '';
+  noResult = false;
+  searchPriceDown: string | null | undefined = '';
+  searchPriceUp: string | null | undefined = '';
 
 
-  constructor(private adsService:AdsService, public loaderService: LoaderService) { }
+  constructor(private adsService: AdsService, public loaderService: LoaderService) { }
 
   ngOnInit(): void {
     this.adsService.loadAds()
-    .pipe(
-      catchError(error => {
+      .pipe(
+        catchError(error => {
           this.errorMsg = error.message;
           return of([]);
-      })) 
-    .subscribe({
-      next: (value) => {
-        this.ads = value;
-        this.filteredAds = this.ads;
-        this.filteredAdsP = this.filteredAds;
-        this.loading = false;
-      }
-    });
+        }))
+      .subscribe({
+        next: (value) => {
+          this.ads = value;
+          this.filteredAds = this.ads;
+          this.loading = false;
+          this.searchTerm = this.adsService.setSearchBar();
+          this.search();
+        }
+      });
+
     
-    this.searchTerm = this.adsService.setSearchBar();
   }
 
-  search(value: string | null | undefined): void {
+   search(): void {
+    let max = Number.MAX_SAFE_INTEGER;
+    let min = 0;
+    console.log(this.searchPriceUp !== null && this.searchPriceUp !== "");
+    if (this.searchPriceUp !== null && this.searchPriceUp !== "") {
+      max = Number(this.searchPriceUp)
+    }
+    if (this.searchPriceDown !== null && this.searchPriceDown !== "") {
+      min = Number(this.searchPriceDown)
+    }
+    if (this.searchTerm == null || this.searchTerm == undefined) {
+      this.searchTerm = "";
+    }
     this.filteredAds = this.ads.filter((val) =>
-      val.title.toLowerCase().includes(value!.toLocaleLowerCase())
+      val.title.toLowerCase().includes(this.searchTerm!.toLocaleLowerCase()) && Number(val.price) >= min && Number(val.price) <= max
     );
-    this.filteredAdsP = this.filteredAds;    
   }
-
-  searchPrice(): void {
-      let max = Number.MAX_SAFE_INTEGER;
-      let min = 0;
-      console.log(this.searchPriceUp !== null && this.searchPriceUp !== "");
-      if(this.searchPriceUp !== null && this.searchPriceUp !== ""){
-        max = Number(this.searchPriceUp)
-      }
-      if(this.searchPriceDown !== null && this.searchPriceDown !== ""){
-        min = Number(this.searchPriceDown)
-      }
-      this.filteredAdsP = this.filteredAds.filter((v) => Number(v.price) >= min && Number(v.price) <= max);
-  }
-
-  
 }
